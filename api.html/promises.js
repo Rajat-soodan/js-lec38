74f280d82bae8ffe2164274346bcea4000e79662
Rajat-soodan <110057447+Rajat-soodan@users.noreply.github.com>
2024-11-29 07:28:55 +0000

    modified:   api.html/promises.js

--- api.html/promises.js
+++ api.html/promises.js
@@ -23,3 +23,53 @@ new Promise(function(resolve,reject){
     console.log("Async 2 resolved");
     
 })
+
+const promiseThree=new Promise(function(resolve,reject){
+    setTimeout(function(){
+      resolve({username:"chai",email:"chai@code.com"}) // think data is coming form somewhere and we have to pass something to execution part then we use data inside it
+    },1000)// data can bein an aform like array object  
+})
+promiseThree.then(function(user){//this user is the what we are accepting as input here the object
+     console.log(user);//to fetch user values output is used
+     })
+const promiseFour= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true
+        if(!error){
+            resolve({username: "hitesh",password:"123"})
+        }
+        else{
+            reject(`ERROR: Something went wrong`)
+        }
+    },1000)
+})
+
+ promiseFour
+ .then((user)=>{
+       console.log(user);
+       return user.username//  we cant directly use console log so we need this
+})  // this return cant be directly fetched to do so we need .then for execution
+.then((uername)=>{
+   console.log(username);
+   
+}).
+catch(function(error){
+    console.log(error);
+    
+}).finally(()=>console.log("The proimse is either resolved or rejected "))
+
+const promiseFive=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true
+        if(!error){
+            resolve({username: "javascript",password:"123"})
+        }
+        else{
+            reject(`ERROR: Js went wrong`)
+        }
+    },1000)
+});
+// it watis for sometime to work to be completed and then waits therr and throws error
+//the issue is we cant gracefully catch 
+
+    
