ef530aeda43ccb6888de2db1fea85cd9afad1e7b
Rajat-soodan <110057447+Rajat-soodan@users.noreply.github.com>
2024-11-30 12:31:37 +0000

    modified:   api.html/oops1.js

--- api.html/oops1.js
+++ api.html/oops1.js
@@ -12,8 +12,33 @@ const user={
     }
 }
 
-console.log(user.username);
-console.log(user.getUserDetails());
-console.log(this);//output-{}// in brrowser it prints window object as whole
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);//output-{}// in brrowser it prints window object as whole
 
+
+const promiseOne=new Promise() // here new is an instance used to create multiple objects of an literal
+ 
+const date=new Date()
+
+function User(username,logincount,isLoggedIn){
+  this.username=username //left hand side is the variable and right hand side is what you are passing
+  this.logincount=logincount
+  this.isLoggedIn=isLoggedIn
+  this.greeting=function(){
+    console.log(`welcome ${this.username}`);
+    
+  }
+  return this //the object is passedon and this can access theese values here
+}
+
+const userOne= User("hitesh",12,true)
+const userTwo=User("ChaiAurCode",11, false) //usertwo over write the values of user 1
+
+console.log(userOne);
+
+// new keyword cretea snew object called instance
+// then this cosntructor function is being called because of new keywords
+// and it then packs a copy of arguments with it
+// this keyword values are injected to this and then returned in fourth step
 
