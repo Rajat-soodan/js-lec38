6b294686c2aad88aba92f1b1eca79437311984cd
Rajat-soodan <110057447+Rajat-soodan@users.noreply.github.com>
2024-12-01 17:37:04 +0000

    modified:   classes_and_oop/Prototype.js

--- classes_and_oop/Prototype.js
+++ classes_and_oop/Prototype.js
@@ -1,5 +1,23 @@
- let myName="hitesh     "
- let myChannel="chai    "
+ //  let myName="hitesh     "
+//  let myChannel="chai    "
 
- console.log(myName.trueLength());
- 
+//  console.log(myName.trueLength());
+
+ let myHeros=["thor","spiderman"]
+
+ let heroPower={
+    thor:"hammer",
+    spiderman:"sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+ }
+ Object.prototype.hitesh=function() {
+    console.log(`hitesh is present in all objects`);
+}
+
+Array.prototype
+//heroPower.hitesh();
+
+ myHeros.hitesh()
