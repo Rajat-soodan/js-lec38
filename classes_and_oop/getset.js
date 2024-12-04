0c886fe3f55c7ea7597b328c83ef4ed3cd333206
Rajat-soodan <110057447+Rajat-soodan@users.noreply.github.com>
2024-12-04 09:59:10 +0000

    modified:   classes_and_oop/getset.js

--- classes_and_oop/getset.js
+++ classes_and_oop/getset.js
@@ -5,12 +5,21 @@ class User{
   }
 
   get password(){
-     return this.password.toUpperCase()
+     //return this._password.toUpperCase()
+     return `${this._password}hitehs`
   }
   set password(value){
-  this.password=value
+        this._password=value
   }
-}
+  get email(){
+    return this._email.toUpperCase()
+  }
+  set email(value){
+      this._email=value
+  }
+} 
+//so now here we are overwriting the password in the constructor 
+// and we are overwriting it here 
 const hit=new User("hitesh.ai","3234")
 console.log(hit.password);
 //consider we dont want everyone to access the pasword in that sceneario we use getter and setter to do the changes
